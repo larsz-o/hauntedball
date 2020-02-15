1d31a601f35916dea0118323c14df8351fb69c51
Lars <larsmackenzie@gmail.com>
2020-02-15 14:04:04 -0500

    add enemy shooting and damage

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+    [SerializeField] float shotSpeed;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemyWeapon;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotSpeed = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownAndShoot();
+    }
+    private void CountDownAndShoot()
+    {
+        shotSpeed -= Time.deltaTime;
+        if (shotSpeed <= 0f)
+        {
+            Fire();
+            shotSpeed = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+    private void Fire() 
+    {
+        GameObject weapon = Instantiate(enemyWeapon, transform.position, Quaternion.identity) as GameObject;
+        weapon.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+    }
+    private void OnTriggerEnter2D(Collider2D thingThatBumpedIntoMe)
+    {
+        DamageDealer damageDealer = thingThatBumpedIntoMe.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+           health -= damageDealer.GetDamage();
+        if (health <= 0) 
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/WeaponShredder.cs
+++ Assets/Scripts/WeaponShredder.cs
@@ -8,7 +8,6 @@ public class WeaponShredder : MonoBehaviour
 
   void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("I am destroying");
         Destroy(collision.gameObject);
     }
 }
